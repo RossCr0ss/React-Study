5e3bce371cb2caf9ee9df0fd289442b78eccc2a0
RossCr0ss <sobechko.r@gmail.com>
2020-09-21 11:49:10 +0300

    add components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const el = (
-  <div>
-    <h1>Todo List</h1>
-    <input type="search" placeholder="type something" />
+const AppHeader = () => {
+  return <h1>Todo List</h1>;
+};
+
+const SeacrhPanel = () => {
+  return <input type="search" placeholder="type something" />;
+};
+
+const TodoList = () => {
+  return (
     <ul>
       <li>Learn React</li>
       <li>Build Awesome App</li>
     </ul>
-  </div>
-);
+  );
+};
 
-ReactDOM.render(el, document.getElementById("root"));
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SeacrhPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));

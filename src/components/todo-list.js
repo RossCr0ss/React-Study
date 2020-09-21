15c1a3083e4e27abe143b65553cf0c85ee0693ee
RossCr0ss <sobechko.r@gmail.com>
2020-09-21 15:51:52 +0300

    change todos data

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-import TodoListItem from './todo-list-item';
+import TodoListItem from "./todo-list-item";
 
-const TodoList = () => {
-  return (
-    <ul>
-      <li><TodoListItem label="Drink Coffee" /></li>
-      <li><TodoListItem label="Build React App" important /></li>
-    </ul>
-  );
+const TodoList = ({ todos }) => {
+  const elements = todos.map((item) => {
+    return (
+      <li>
+        <TodoListItem {...item} />
+      </li>
+    );
+  });
+  return <ul>{elements}</ul>;
 };
 
 export default TodoList;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import AppHeader from './components/app-header';
 import SeacrhPanel from './components/search-panel';
 import TodoList from './components/todo-list';
 
+
+const todoData = [
+  {label: 'Drink Coffee', important: false},
+  {label: 'Build Awesome App', important: true},
+  {label: 'Learn React', important: false}
+];
+
 const App = () => {
   return (
     <div>
       <AppHeader />
       <SeacrhPanel />
-      <TodoList />
+      <TodoList todos= {todoData}/>
     </div>
   );
 };
